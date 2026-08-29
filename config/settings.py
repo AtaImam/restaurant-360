746318c0a607658cf232651ae41cdb1dce34e58a
Ata Imam Hossain <jisanata761@gmail.com>
2026-08-29 22:00:35 +0600

    Add table QR code generation

--- config/settings.py
+++ config/settings.py
@@ -24,8 +24,11 @@ SECRET_KEY = 'django-insecure-u60hq_%9s1p)6#mtpbad++o0d_%nhb1eelfkhky0j6mb$gb#6&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '172.20.10.5',
+]
 
 
 # Application definition
@@ -118,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 # Email
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,6 +1,8 @@
+import qrcode
+from io import BytesIO
+
 from django.db import models
-
-
+from django.core.files import File
 class Restaurant(models.Model):
     name = models.CharField(max_length=150)
     address = models.TextField()
@@ -16,8 +18,37 @@ class Table(models.Model):
         on_delete=models.CASCADE,
         related_name='tables'
     )
+
     table_number = models.PositiveIntegerField()
+
     is_active = models.BooleanField(default=True)
+
+    qr_code = models.ImageField(
+        upload_to='qr_codes/',
+        blank=True,
+        null=True
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if not self.qr_code:
+            menu_url = f"http://172.20.10.5:8000/menu/{self.restaurant.id}/table/{self.id}/"
+
+            qr = qrcode.make(menu_url)
+
+            buffer = BytesIO()
+            qr.save(buffer, format="PNG")
+
+            file_name = f"table_{self.id}_qr.png"
+
+            self.qr_code.save(
+                file_name,
+                File(buffer),
+                save=False
+            )
+
+            super().save(update_fields=["qr_code"])
 
     def __str__(self):
         return f"{self.restaurant.name} - Table {self.table_number}"
